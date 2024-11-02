52dbeb6d6fc813dd8e9d074fdc631b9cc4729968
shandy <shandyfrtnaaa@gmail.com>
2024-11-02 10:47:09 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
+  
